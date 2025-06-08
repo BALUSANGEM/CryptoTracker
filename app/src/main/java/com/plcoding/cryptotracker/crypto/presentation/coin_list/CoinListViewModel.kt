511bca0a336861a7e1149c6eb60818f9b314eded
BALUSANGEM <vishnubalu.sangem@gmail.com>
2025-06-08 20:57:58 +0530

    Show errors as toast

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -6,9 +6,11 @@ import com.plcoding.cryptotracker.core.domain.util.onError
 import com.plcoding.cryptotracker.core.domain.util.onSuccess
 import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
 import com.plcoding.cryptotracker.crypto.presentation.models.toCoinUi
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -27,10 +29,8 @@ class CoinListViewModel(
             CoinListState()
         )
 
-    init {
-        loadCoins()
-    }
-
+    private val _events = Channel<CoinListEvent>()
+    val events = _events.receiveAsFlow()
 
     fun onAction(action: CoinListAction) {
         when (action) {
@@ -58,6 +58,7 @@ class CoinListViewModel(
                     _state.update {
                         it.copy(isLoading = false)
                     }
+                    _events.send(CoinListEvent.Error(error))
                 }
         }
     }
