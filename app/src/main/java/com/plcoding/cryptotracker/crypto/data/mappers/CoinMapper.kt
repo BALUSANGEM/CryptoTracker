35d2813be2e808a693e3cb74225970d09d632192
BALUSANGEM <vishnubalu.sangem@gmail.com>
2025-06-21 20:56:16 +0530

    Implement LineChart and show price history as Line Chart

--- app/src/main/java/com/plcoding/cryptotracker/crypto/data/mappers/CoinMapper.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/data/mappers/CoinMapper.kt
@@ -24,6 +24,6 @@ fun CoinPriceDto.toCoinPrice(): CoinPrice {
         priceUsd = priceUsd,
         daeTime = Instant
             .ofEpochMilli(time)
-            .atZone(ZoneId.of("UTC"))
+            .atZone(ZoneId.systemDefault())
     )
 }
